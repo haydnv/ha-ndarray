66c514ebf39be033dfd2befbeb54e1b441eea00e
haydnv <haydnv@gmail.com>
2023-01-20 00:07:54 +0530

    add cuda feature

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
 fn main() {
+    #[cfg(feature = "cuda")]
+    pkg_config::Config::new()
+        .atleast_version("11.0")
+        .probe("cuda")
+        .unwrap();
+
     #[cfg(feature = "opencl")]
     pkg_config::Config::new()
         .atleast_version("3.0")
