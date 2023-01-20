6f645db34ff6edc07c69ebd60b6f499235c299c8
Haydn Vestal <haydnv@gmail.com>
2023-01-20 21:38:44 +0530

    add test for opencl

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,27 @@
 #[cfg(test)]
 mod tests {
-    use custos::CPU;
     use custos_math::Matrix;
 
     #[test]
-    fn it_works() {
+    fn test_cpu() {
+        use custos::CPU;
+
         let device = CPU::new();
+
+        let a = Matrix::from((&device, (2, 3), [1., 2., 3., 4., 5., 6.]));
+        let b = Matrix::from((&device, (3, 2), [6., 5., 4., 3., 2., 1.]));
+
+        let c = a.gemm(&b);
+
+        assert_eq!(c.read(), vec![20., 14., 56., 41.,]);
+    }
+
+    #[cfg(feature = "opencl")]
+    #[test]
+    fn test_opencl() {
+        use custos::CLDevice;
+
+        let device = CLDevice::new(0).expect("OpenCL device");
 
         let a = Matrix::from((&device, (2, 3), [1., 2., 3., 4., 5., 6.]));
         let b = Matrix::from((&device, (3, 2), [6., 5., 4., 3., 2., 1.]));
