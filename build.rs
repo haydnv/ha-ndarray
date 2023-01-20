ff7268f8dec60d109400ae20419a582539508120
Haydn Vestal <haydnv@gmail.com>
2023-01-20 21:09:40 +0530

    remove CUDA feature until dependency conflicts can be worked out

--- build.rs
+++ build.rs
@@ -1,10 +1,4 @@
 fn main() {
-    #[cfg(feature = "cuda")]
-    pkg_config::Config::new()
-        .atleast_version("11.0")
-        .probe("cuda")
-        .unwrap();
-
     #[cfg(feature = "opencl")]
     pkg_config::Config::new()
         .atleast_version("3.0")
