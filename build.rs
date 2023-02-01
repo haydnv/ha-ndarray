050bede2768db07a8ec73be6d96d932e0c0195eb
Haydn Vestal <haydnv@gmail.com>
2023-02-01 19:04:01 +0530

    add Dockerfile to support tests for CPU, CUDA, and OpenCL features

--- build.rs
+++ build.rs
@@ -4,4 +4,9 @@ fn main() {
         .atleast_version("3.0")
         .probe("OpenCL")
         .unwrap();
+
+    #[cfg(feature = "cuda")]
+    {
+        // CUDA does not provide a .pc file for use with pkg-config, so there's no check to run here
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod tests {
     use custos_math::Matrix;
 
     #[test]
-    fn test_cpu() {
+    fn test_matmul_cpu() {
         use custos::CPU;
 
         let device = CPU::new();
@@ -18,10 +18,25 @@ mod tests {
 
     #[cfg(feature = "opencl")]
     #[test]
-    fn test_opencl() {
+    fn test_matmul_opencl() {
         use custos::CLDevice;
 
         let device = CLDevice::new(0).expect("OpenCL device");
+
+        let a = Matrix::from((&device, (2, 3), [1., 2., 3., 4., 5., 6.]));
+        let b = Matrix::from((&device, (3, 2), [6., 5., 4., 3., 2., 1.]));
+
+        let c = a.gemm(&b);
+
+        assert_eq!(c.read(), vec![20., 14., 56., 41.,]);
+    }
+
+    #[cfg(feature = "cuda")]
+    #[test]
+    fn test_matmul_cuda() {
+        use custos::CudaDevice;
+
+        let device = CudaDevice::new(0).expect("CUDA device");
 
         let a = Matrix::from((&device, (2, 3), [1., 2., 3., 4., 5., 6.]));
         let b = Matrix::from((&device, (3, 2), [6., 5., 4., 3., 2., 1.]));
